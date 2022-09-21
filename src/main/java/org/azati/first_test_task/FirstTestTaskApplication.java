3f044d4f098e2ec0a9bf576c4bc2e70527895a9f
kaerimashou <reajlcade96@gmail.com>
2022-09-21 17:24:07 +0300

    Initial Commit

---
+++ src/main/java/org/azati/first_test_task/FirstTestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.azati.first_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstTestTaskApplication.class, args);
+    }
+
+}
